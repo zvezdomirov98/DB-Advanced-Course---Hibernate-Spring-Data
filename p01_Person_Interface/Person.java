28beaa4e5c660b2b8947b9befaeae5e5e896a3ec
zvezdomirov98 <30501998+zvezdomirov98@users.noreply.github.com>
2019-02-28 10:55:24 +0200

    Add files via upload

---
+++ p01_Person_Interface/Person.java
@@ -0,0 +1,6 @@
+package p01_Person_Interface;
+
+public interface Person {
+    String getName();
+    int getAge();
+}
---
+++ p03_Ferrari/Car.java
@@ -0,0 +1,9 @@
+package p03_Ferrari;
+
+public interface Car {
+    String getModel();
+    String getBrand();
+    String getDriverName();
+    String pushBrakePedal();
+    String pushGasPedal();
+}
