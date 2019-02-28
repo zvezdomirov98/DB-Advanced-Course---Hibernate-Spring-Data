78df02633d7d4103ba77eb7aec895e20a47883d2
zvezdomirov98 <30501998+zvezdomirov98@users.noreply.github.com>
2019-02-28 11:00:49 +0200

    Delete Car.java

--- p03_Ferrari/Car.java
+++
@@ -1,9 +0,0 @@
-package p03_Ferrari;
-
-public interface Car {
-    String getModel();
-    String getBrand();
-    String getDriverName();
-    String pushBrakePedal();
-    String pushGasPedal();
-}
