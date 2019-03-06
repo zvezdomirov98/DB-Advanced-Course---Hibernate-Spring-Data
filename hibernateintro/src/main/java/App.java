4df3faca3fb5b0e33eb72e57c3e5b65ef2bdb176
zvezdomirov98 <30501998+zvezdomirov98@users.noreply.github.com>
2019-03-06 06:13:37 +0200

    Add files via upload

--- hibernateintro/src/main/java/App.java
+++ hibernateintro/src/main/java/App.java
@@ -7,16 +7,33 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class App {
+
+    private static EntityManager em;
+
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence
                 .createEntityManagerFactory("soft_uni");
-        EntityManager em = emf.createEntityManager();
+        em = emf.createEntityManager();
+    }
+
+
+    /*Problem 1: Remove Objects*/
+    private static void setLongNamesToLower() {
+        String hqlQuery = "FROM Town";
         em.getTransaction().begin();
-        List<Town> result = em.createQuery("FROM Town ", Town.class).getResultList();
+        List<Town> result = em.createQuery(hqlQuery, Town.class).getResultList();
         result
-                .forEach(System.out::println);
+                .forEach(t -> {
+                    if (t.getName().length() > 5) {
+                        em.detach(t);
+                    } else {
+                        t.setName(t.getName().toLowerCase());
+                        em.persist(t);
+                    }
+                });
         em.getTransaction().commit();
     }
 }
